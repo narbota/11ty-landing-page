f11c9609a1e4218dc9913d8d251e881af3e459bb
Andrea N <andrea@narbot.com>
2023-02-15 05:59:33 -0600

    Update CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
   purge: [],
   presets: [],
-  darkMode: true // or 'media' or 'class'
+  darkMode: false // or 'media' or 'class'
   theme: {
     screens: {
       sm: '640px',
